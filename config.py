287e28c30e2d0e21c7937f4b16c219b14b4c2380
Sergio Garcia <74437121+sergiogcdev@users.noreply.github.com>
2022-04-09 07:51:18 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 # Copyright (c) 2010 Aldo Cortesi
 # Copyright (c) 2010, 2014 dequis
 # Copyright (c) 2012 Randall Ma
@@ -25,136 +24,89 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from libqtile.config import Key, Screen, Group, Drag, Click
+from libqtile import bar, layout, widget
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile import layout, bar, widget
-from typing import List  # noqa: F401
+from libqtile.utils import guess_terminal
 
 mod = "mod4"
 
 keys = [
     # Custom layout keys
-    # XMonadTall
-    Key([mod], "h", lazy.layout.left()),
-    Key([mod], "l", lazy.layout.right()),
-    Key([mod], "j", lazy.layout.down()),
-    Key([mod], "k", lazy.layout.up()),
-    Key([mod, "shift"], "h", lazy.layout.swap_left()),
-    Key([mod, "shift"], "l", lazy.layout.swap_right()),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
-    Key([mod, "control"], "k", lazy.layout.grow()),
-    Key([mod, "control"], "j", lazy.layout.shrink()),
-    Key([mod, "shift"], "n", lazy.layout.normalize()),
-    # Key([mod], "n", lazy.layout.maximize()),
-    Key([mod, "shift"], "space", lazy.layout.flip()),
-    Key([mod], "Return", lazy.layout.toggle_split()),
-    # Bsp
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left()),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right()),
-    Key([mod, "mod1"], "j", lazy.layout.flip_down()),
-    Key([mod, "mod1"], "k", lazy.layout.flip_up()),
-    Key([mod, "mod1"], "h", lazy.layout.flip_left()),
-    Key([mod, "mod1"], "l", lazy.layout.flip_right()),
-    Key([mod, "control"], "j", lazy.layout.grow_down()),
-    Key([mod, "control"], "k", lazy.layout.grow_up()),
-    Key([mod, "control"], "h", lazy.layout.grow_left()),
-    Key([mod, "control"], "l", lazy.layout.grow_right()),
-    # Switch between windows in current stack pane
-    # Key([mod], "k", lazy.layout.down()),
-    # Key([mod], "j", lazy.layout.up()),
-
-    # Move windows up or down in current stack
-    # Key(["mod1", "shift"], "k", lazy.layout.shuffle_down()),
-    # Key(["mod1", "shift"], "j", lazy.layout.shuffle_up()),
-
-    # Switch window focus to other pane(s) of stack
-    Key(["mod1"], "Tab", lazy.layout.next()),
-
-    
-    # Swap panes of split stack
-    # Key([mod, "shift"], "space", lazy.layout.rotate()),
-
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    # Key([mod, "shift"], "s", lazy.layout.toggle_split()),
-    Key(["control", "mod1"], "t", lazy.spawn("tilix")),
-
-    # Run rofi
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([mod], "Left", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod], "Right", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod], "Down", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "shift"], "Left", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "shift"], "Right", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "shift"], "Down", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "shift"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod, "shift"], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([mod, "shift"], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key(["control", "mod1"], "t", lazy.spawn("kitty")),
     Key(["mod1"], "w", lazy.spawn("rofi -show window -theme gruvbox-dark")),
     Key(["mod1"], "r", lazy.spawn("rofi -show run -theme gruvbox-dark")),
-
-    # Toggle between different layouts as defined below
+    Key(["mod1"], "e", lazy.spawn("rofi -show emoji -modi emoji -theme gruvbox-dark")),
     Key([mod], "Return", lazy.next_layout()),
-    # default closing windows:    Key([mod], "w", lazy.window.kill()),
-
     Key([mod, "control"], "r", lazy.restart()),
     Key([mod, "control"], "q", lazy.shutdown()),
-    Key([mod], "r", lazy.spawncmd()),
-
-    # Close Window
-    Key(["mod1"], "F4", lazy.window.kill()),
-
-    # Change Wallpaper
-    Key([mod], "r", lazy.spawn("python /home/sergio/Scripts/change_wallpaper.py")),
+    Key([mod, "shift"], "r", lazy.spawncmd()),
+    Key([mod], "r", lazy.spawn("python3 /home/sergio/Scripts/change_wallpaper.py")),
 
     # Volume
     Key(["mod1"], "F3", lazy.spawn("pamixer -i 3")),
     Key(["mod1"], "F2", lazy.spawn("pamixer -d 3")),
     Key(["mod1"], "F1", lazy.spawn("pamixer -t")),
 
-    # Play Youtube Videos in High Quality
-    Key([mod], "y", lazy.spawn("/home/sergio/Scripts/Bash/youtube_mpv.sh")),
-
     # Mute and unmute microphone
     Key([mod], "m", lazy.spawn("pamixer --source 2 -m")),
     Key([mod, "shift"], "m", lazy.spawn("pamixer --source 2 -u")),
 
-    # Lock screen
-    Key([mod], "x", lazy.spawn("i3lock -c 252525")),
-
     # Minimize & float window
     Key([mod], "w", lazy.window.toggle_minimize()),
     Key([mod, "shift"], "w", lazy.window.toggle_floating()),
-
-    # Open dmenu
-    Key([mod], "b", lazy.spawn("dmenu_run")),
-
-    # Transparency
-    # Key([mod, "control"], "t", lazy.spawn("picom-trans -c -7")),
-    # Key([mod, "control"], "y", lazy.spawn("picom-trans -c +7")),
-
-    # Open pdf without file manager
-    Key([mod, "shift"], "b", lazy.spawn("/home/sergio/Scripts/Bash/open_pdf.sh")),
-
-    # Open jgmenu
-    Key(["control", "mod1"], "j", lazy.spawn("jgmenu_run")),
 ]
 
 groups = [Group(i) for i in "asdfuiop"]
 
 for i in groups:
-    keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen()),
-
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
-    ])
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod1 + shift + letter of group = switch to & move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+            # Or, use below if you prefer not to switch to that group.
+            # # mod1 + shift + letter of group = move focused window to group
+            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+            #     desc="move focused window to group {}".format(i.name)),
+        ]
+    )
 
 layouts = [
-    # layout.MonadTall(border_focus='green', border_normal='#4E4A4A', border_width=3, margin=5),
-    layout.Bsp(border_focus='green', border_normal='#4E4A4A', border_width=3, margin=5),
+    layout.Bsp(border_focus='green', border_normal='#4E4A4A', border_width=0, margin=5),
     layout.Floating(),
     layout.Max(),
-    # layout.Stack(num_stacks=2),
+    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    # layout.Max(),
     # Try more layouts by unleashing below layouts.
-    # layout.Columns(),
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
     # layout.Matrix(),
     # layout.MonadTall(),
     # layout.MonadWide(),
@@ -164,10 +116,7 @@ layouts = [
     # layout.VerticalTile(),
     # layout.Zoomy(),
 ]
-##### COLORS #####
-#colors = [
-#          ["#1a1a1a", "#000000"], # font color for group names
-#         ]
+
 
 colors = [["#292d3e", "#292d3e"], # panel background
           ["#434758", "#434758"], # background for current screen tab
@@ -190,11 +139,12 @@ colors_two = {
     "orange":		["#EBCB8B", "#EBCB8B"]
 }
 
+
 widget_defaults = dict(
-    font='DaddyTimeMono Nerd Font Mono',
+    font="DaddyTimeMono Nerd Font Mono",
     fontsize=14,
-    padding=3,
-    background=colors[0],
+    padding=5,
+    background=colors[0]
 )
 extension_defaults = widget_defaults.copy()
 
@@ -260,17 +210,13 @@ screens = [
         			filename = "/home/sergio/.config/qtile/icons/bar05.png",
         			background = colors_two["blue"]
         		),
-                widget.Wlan(
+                widget.Memory(
                     background = colors_two["blue"],
                     foreground = colors_two["black"],
-                    interface='wlp3s0'
                 ),
             ],
             24,
-	    #background="#0d0d0d",
         ),
-	#wallpaper='/home/sergio/Pictures/wallpapers/anime-girl-01.jpg',
-	#wallpaper_mode='fill',
     ),
     Screen(
         top=bar.Bar(
@@ -306,7 +252,6 @@ screens = [
                     background = colors_two["red"],
                     foreground = colors_two["black"]
                 ),
-                widget.Systray(),
                 widget.Image(
                     scale = True,
                     filename = "/home/sergio/.config/qtile/icons/bar03.png",
@@ -333,54 +278,49 @@ screens = [
         			filename = "/home/sergio/.config/qtile/icons/bar05.png",
         			background = colors_two["blue"]
         		),
-                widget.Wlan(
+                widget.Memory(
                     background = colors_two["blue"],
-                    foreground = colors_two["black"],
-                    interface='wlp3s0'
-                ),
+                    foreground = colors_two["black"]                ),
             ],
             24,
-	    #background="#0d0d0d",
         ),
-	#wallpaper='/home/sergio/Pictures/wallpapers/anime-girl-01.jpg',
-	#wallpaper_mode='fill',
     ),
 ]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
 dgroups_key_binder = None
-dgroups_app_rules = []  # type: List
-main = None
+dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-floating_layout = layout.Floating(float_rules=[
-    # Run the utility of `xprop` to see the wm class and name of an X client.
-    {'wmclass': 'confirm'},
-    {'wmclass': 'dialog'},
-    {'wmclass': 'download'},
-    {'wmclass': 'error'},
-    {'wmclass': 'file_progress'},
-    {'wmclass': 'notification'},
-    {'wmclass': 'splash'},
-    {'wmclass': 'toolbar'},
-    {'wmclass': 'confirmreset'},  # gitk
-    {'wmclass': 'makebranch'},  # gitk
-    {'wmclass': 'maketag'},  # gitk
-    {'wname': 'branchdialog'},  # gitk
-    {'wname': 'pinentry'},  # GPG key password entry
-    {'wmclass': 'ssh-askpass'},  # ssh-askpass
-])
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
 auto_fullscreen = True
 focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
