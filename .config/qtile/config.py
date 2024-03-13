a161a926048ea11ae4a5e7edefb0ac202c511881
sergio <sergio@debian.sergigarcia.cat>
2024-03-13 16:44:37 +0100

    Add stick/unstick window function

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,32 +1,26 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+###################################################
+#  mmmm  m   ""#                     m"" "        #
+# m"  "mm#mmmm #  mmm    mmm mmmm mmm#mmmm  mmmm  #
+# #    # #   # # #"     #"  #" "#"  ##   # #" "#  #
+# #    # #   # # #"""   #   #   #   ##   # #   #  #
+#  #mm#" "mmm#m"m"#mm   "#mm"#m##   ## mm#m"#m"#  #
+#     #                                     m  #  #
+#                                            ""   #
+#   By: B34n5                                     # 
+###################################################
 
-from libqtile import bar, layout, qtile, widget
+from libqtile import bar, layout, qtile, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen, EzKey
 from libqtile.lazy import lazy
+
+win_list = []
+def stick_win(qtile):
+    global win_list
+    win_list.append(qtile.current_window)
+def unstick_win(qtile):
+    global win_list
+    if qtile.current_window in win_list:
+        win_list.remove(qtile.current_window)
 
 mod = "mod1"
 terminal = "kitty"
@@ -69,6 +63,10 @@ keys = [
     EzKey("A-s", lazy.spawn("scrot /home/sergio/pictures/screenshots/scrot_%y-%m-%d_%H%M%S.png"), desc="Take screenshot"),
     EzKey("A-C-s", lazy.spawn("scrot -s /home/sergio/pictures/screenshots/scrot_%y-%m-%d_%H%M%S.png"), desc="Take a selected screenshot"),
     EzKey("A-S-s", lazy.spawn("kitty screenRecorder"), desc="Capture desktop"),
+    ###################################
+    # lazy custom function
+    EzKey("C-A-c", lazy.function(stick_win), desc="stick win"),
+    EzKey("C-A-S-c", lazy.function(unstick_win), desc="unstick win"),
     ###################################
     # Scripts
     EzKey("M-q", lazy.spawn("/home/sergio/scripts/switch-qtile"), desc=".xinitrc symlink points to qtilerc"),
@@ -114,6 +112,11 @@ layouts = [
     # layout.VerticalTile(),
     # layout.Zoomy(),
 ]
+
+@hook.subscribe.setgroup
+def move_win():
+    for w in win_list:
+        w.togroup(qtile.current_group.name)
 
 widget_defaults = dict(
     font="JetBrains Mono",
